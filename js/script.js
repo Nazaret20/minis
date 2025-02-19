7b941eaf9605c8608abbbbb5e00f253cbca8aef2
Sudev <butmssblai@gmail.com>
2025-02-19 17:36:05 +0100

    fix theme

--- js/script.js
+++ js/script.js
@@ -1,44 +1,71 @@
-document.addEventListener("DOMContentLoaded", () => {
-	function contadorDeClics() {
-		const botonClics = document.querySelector(".boton-raton");
-		const numeroDeClics = document.querySelector("#numero-clics");
-		let clics = 0;
-		botonClics.addEventListener("click", () => {
-			console.log("picándole");
-			clics++;
-			numeroDeClics.textContent = clics;
-		});
-	}
-	contadorDeClics();
+document.addEventListener('DOMContentLoaded', () => {
+    tema();
 
-	function relojDigital() {
-		const relojP = document.querySelector("#reloj");
-		let fecha = new Date();
-		let hora = fecha.getHours();
-		let minutos = fecha.getMinutes();
-		let segundos = fecha.getSeconds();
+    function contadorDeClics() {
+        const botonClics = document.querySelector('.boton-raton');
+        const numeroDeClics = document.querySelector('#numero-clics');
+        let clics = 0;
+        botonClics.addEventListener('click', () => {
+            console.log('picándole');
+            clics++;
+            numeroDeClics.textContent = clics;
+        });
+    }
+    contadorDeClics();
 
-		if (minutos < 10) {
-			minutos = "0" + minutos;
-		}
-		if (segundos < 10) {
-			segundos = "0" + segundos;
-		}
+    function relojDigital() {
+        const relojP = document.querySelector('#reloj');
+        let fecha = new Date();
+        let hora = fecha.getHours();
+        let minutos = fecha.getMinutes();
+        let segundos = fecha.getSeconds();
 
-		relojP.textContent = `${hora}:${minutos}:${segundos}`;
-	}
-	relojDigital();
-	setInterval(relojDigital, 1000);
+        if (minutos < 10) {
+            minutos = '0' + minutos;
+        }
+        if (segundos < 10) {
+            segundos = '0' + segundos;
+        }
 
-	function tema() {
-		const botonTema = document.querySelector(".boton-tema");
-		const body = document.body;
+        relojP.textContent = `${hora}:${minutos}:${segundos}`;
+    }
+    relojDigital();
+    setInterval(relojDigital, 1000);
 
-		botonTema.addEventListener("click", () => {
-			body.classList.toggle("light");
-			body.classList.toggle("dark");
-
-		});
-	}
-	tema();
+    function tema() {
+        const botonTema = document.querySelector('.boton-tema');
+        const iconoTema = botonTema.querySelector('img');
+        const body = document.body;
+        let temaGuardado = localStorage.getItem('temas');
+    
+        const darkModeSVG = "./assets/svg/dark.svg";
+        const lightModeSVG = "./assets/svg/light.svg";
+    
+        if (!temaGuardado) {
+            temaGuardado = 'dark';
+            localStorage.setItem('temas', 'dark');
+        }
+    
+        body.classList.add(temaGuardado);
+        iconoTema.src = temaGuardado === 'dark' ? darkModeSVG : lightModeSVG;
+    
+        if (temaGuardado === 'light') {
+            botonTema.classList.add('pulsado');
+        }
+    
+        botonTema.addEventListener('click', () => {
+            body.classList.toggle('light');
+            body.classList.toggle('dark');
+            botonTema.classList.toggle('pulsado'); 
+    
+            if (body.classList.contains('light')) {
+                iconoTema.src = lightModeSVG;
+                localStorage.setItem('temas', 'light');
+            } else {
+                iconoTema.src = darkModeSVG;
+                localStorage.setItem('temas', 'dark');
+            }
+        });
+    }
+    
 });
