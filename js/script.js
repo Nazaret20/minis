f0e34e3db217a8edb684bf8cfb4685f66305d353
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-02-23 12:45:05 +0100

    añadido lógica para no repetir cita

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const numeroDeClics = document.querySelector('#numero-clics');
         let clics = 0;
         botonClics.addEventListener('click', () => {
-            console.log('picándole');
             clics++;
             numeroDeClics.textContent = clics;
         });
@@ -117,53 +116,83 @@ document.addEventListener('DOMContentLoaded', () => {
         const botonCitas = document.querySelector('.boton-citas');
         const pCitas = document.querySelector('.p-citas');
 
-        botonCitas.addEventListener('click', () => {
-            console.log('clic');
-            let numeroRandom = Math.floor(Math.random() * 9);
-            const citas = [
-                {
-                    cita: '"La vida es lo que sucede mientras estás ocupado haciendo otros planes."',
-                    autor: 'John Lennon',
-                },
-                {
-                    cita: '"El éxito es aprender a ir de fracaso en fracaso sin desesperarse."',
-                    autor: 'Winston Churchill',
-                },
-                {
-                    cita: '"Haz de tu vida un sueño, y de tu sueño una realidad."',
-                    autor: 'Antoine de Saint-Exupéry',
-                },
-                {
-                    cita: 'No cuentes los días, haz que los días cuenten.',
-                    autor: 'Muhammad Ali',
-                },
-                {
-                    cita: '"No tengas miedo de renunciar a lo bueno para ir por lo grandioso."',
-                    autor: 'John D. Rockefeller',
-                },
-                {
-                    cita: '"No encontrarás nunca un arcoíris si siempre estás mirando hacia abajo."',
-                    autor: 'Charlie Chaplin',
-                },
-                {
-                    cita: '"No esperes. El tiempo nunca será el justo."',
-                    autor: 'Napoleón Hill',
-                },
-                {
-                    cita: '"Todo parece imposible hasta que se hace."',
-                    autor: 'Nelson Mandela',
-                },
-                {
-                    cita: '"La lógica te llevará de A a B. La imaginación te llevará a cualquier lugar."',
-                    autor: ' Albert Einstein',
-                },
-                {
-                    cita: '"El mayor error que puedes cometer en la vida es tener miedo de cometer errores."',
-                    autor: 'Elbert Hubbard',
-                },
-            ];
+        const citas = [
+            {
+                cita: '"La vida es lo que sucede mientras estás ocupado haciendo otros planes."',
+                autor: 'John Lennon',
+                haSalido: false,
+            },
+            {
+                cita: '"El éxito es aprender a ir de fracaso en fracaso sin desesperarse."',
+                autor: 'Winston Churchill',
+                haSalido: false,
+            },
+            {
+                cita: '"Haz de tu vida un sueño, y de tu sueño una realidad."',
+                autor: 'Antoine de Saint-Exupéry',
+                haSalido: false,
+            },
+            {
+                cita: 'No cuentes los días, haz que los días cuenten.',
+                autor: 'Muhammad Ali',
+                haSalido: false,
+            },
+            {
+                cita: '"No tengas miedo de renunciar a lo bueno para ir por lo grandioso."',
+                autor: 'John D. Rockefeller',
+                haSalido: false,
+            },
+            {
+                cita: '"No encontrarás nunca un arcoíris si siempre estás mirando hacia abajo."',
+                autor: 'Charlie Chaplin',
+                haSalido: false,
+            },
+            {
+                cita: '"No esperes. El tiempo nunca será el justo."',
+                autor: 'Napoleón Hill',
+                haSalido: false,
+            },
+            {
+                cita: '"Todo parece imposible hasta que se hace."',
+                autor: 'Nelson Mandela',
+                haSalido: false,
+            },
+            {
+                cita: '"La lógica te llevará de A a B. La imaginación te llevará a cualquier lugar."',
+                autor: ' Albert Einstein',
+                haSalido: false,
+            },
+            {
+                cita: '"El mayor error que puedes cometer en la vida es tener miedo de cometer errores."',
+                autor: 'Elbert Hubbard',
+                haSalido: false,
+            },
+        ];
 
-            pCitas.innerHTML = `<em>${citas[numeroRandom].cita}</em> - ${citas[numeroRandom].autor}`
+        botonCitas.addEventListener('click', () => {
+            let numeroRandom;
+            let contador = 0;
+
+            for (let i = 0; i < citas.length; i++) {
+                if (citas[i].haSalido === true) {
+                    contador++;
+                }
+            }
+
+            let hanSalidoTodas = contador === citas.length;
+            if (hanSalidoTodas) {
+                for (let i = 0; i < citas.length; i++) {
+                    citas[i].haSalido = false;
+                }
+            }
+
+            do {
+                numeroRandom = Math.floor(Math.random() * citas.length);
+            } while (citas[numeroRandom].haSalido === true);
+
+            citas[numeroRandom].haSalido = true;
+
+            pCitas.innerHTML = `<em>${citas[numeroRandom].cita}</em> - ${citas[numeroRandom].autor}`;
         });
     }
     generarCita();
